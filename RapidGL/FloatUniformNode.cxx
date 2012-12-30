90056cd350bde15af8b70156e33fe7a2eaaa85fd
Andy Brown <adb1413@rit.edu>
2012-12-30 01:23:28 -0500

    Added FloatUniformNode

---
+++ RapidGL/FloatUniformNode.cxx
@@ -0,0 +1,54 @@
+/*
+ * RapidGL - Rapid prototyping for OpenGL
+ * Copyright (C) 2012  Andrew Brown
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "config.h"
+#include "RapidGL/FloatUniformNode.hxx"
+namespace RapidGL {
+
+/**
+ * Constructs a `FloatUniformNode`.
+ *
+ * @param name Name of the uniform as declared in the shader
+ * @throws invalid_argument if name is empty
+ */
+FloatUniformNode::FloatUniformNode(const std::string& name) : UniformNode(name, GL_FLOAT) {
+    // empty
+}
+
+/**
+ * Returns the value of the uniform.
+ *
+ * @return Value of the uniform
+ */
+GLfloat FloatUniformNode::getValue() const {
+    return value;
+}
+
+/**
+ * Changes the value of the uniform.
+ *
+ * @param value New value of uniform
+ */
+void FloatUniformNode::setValue(const GLfloat value) {
+    this->value = value;
+}
+
+void FloatUniformNode::visit(State& state) {
+    glUniform1f(getLocation(), value);
+}
+
+} /* namespace RapidGL */
---
+++ RapidGL/FloatUniformNodeTest.cxx
@@ -0,0 +1,143 @@
+/*
+ * RapidGL - Rapid prototyping for OpenGL
+ * Copyright (C) 2012  Andrew Brown
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "config.h"
+#include <cppunit/extensions/HelperMacros.h>
+#include <GL/glfw.h>
+#include <stdexcept>
+#include "RapidGL/FloatUniformNode.hxx"
+#include "RapidGL/ProgramNode.hxx"
+#include "RapidGL/ShaderNode.hxx"
+#include "RapidGL/State.hxx"
+#include "RapidGL/Visitor.hxx"
+
+
+/**
+ * Unit test for FloatUniformNode.
+ */
+class FloatUniformNodeTest {
+public:
+
+    // Threshold for floating-point comparisons
+    static const double TOLERANCE = 1e-9;
+
+    /**
+     * Returns the source code for the vertex shader.
+     */
+    static std::string getVertexShaderSource() {
+        return
+                "#version 140\n"
+                "in vec4 MCVertex;\n"
+                "void main() {\n"
+                "  gl_Position = MCVertex;\n"
+                "}\n";
+    }
+
+    /**
+     * Returns the source code for the fragment shader.
+     */
+    static std::string getFragmentShaderSource() {
+        return
+                "#version 140\n"
+                "uniform float Opacity;\n"
+                "out vec4 FragColor;\n"
+                "void main() {\n"
+                "  FragColor = vec4(1, 1, 1, Opacity);\n"
+                "}\n";
+    }
+
+    // Program
+    RapidGL::ProgramNode programNode;
+
+    // Vertex shader
+    RapidGL::ShaderNode vertexShaderNode;
+
+    // Fragment shader
+    RapidGL::ShaderNode fragmentShaderNode;
+
+    // Uniform
+    RapidGL::FloatUniformNode uniformNode;
+
+    /**
+     * Constructs the test.
+     */
+    FloatUniformNodeTest() :
+            vertexShaderNode(GL_VERTEX_SHADER, getVertexShaderSource()),
+            fragmentShaderNode(GL_FRAGMENT_SHADER, getFragmentShaderSource()),
+            uniformNode("Opacity") {
+        programNode.addChild(&vertexShaderNode);
+        programNode.addChild(&fragmentShaderNode);
+        programNode.addChild(&uniformNode);
+    }
+
+    /**
+     * Ensures `FloatUniformNode::getType` returns `GL_FLOAT`.
+     */
+    void testGetType() {
+        CPPUNIT_ASSERT_EQUAL((GLenum) GL_FLOAT, uniformNode.getType());
+    }
+
+    /**
+     * Ensures `FloatUniformNode::visit` works correctly.
+     */
+    void testVisit() {
+
+        // Set value
+        uniformNode.setValue(0.5f);
+
+        // Visit the nodes
+        RapidGL::State state;
+        RapidGL::Visitor visitor(&state);
+        visitor.visit(&programNode);
+
+        // Check value
+        const Gloop::Program program = programNode.getProgram();
+        GLfloat actual;
+        glGetUniformfv(program.id(), uniformNode.getLocation(), &actual);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5f, actual, TOLERANCE);
+    }
+};
+
+int main(int argc, char* argv[]) {
+
+    // Initialize GLFW
+    if (!glfwInit()) {
+        throw std::runtime_error("Could not initialize GLFW!");
+    }
+
+    // Open window
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
+    glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    if (!glfwOpenWindow(512, 512, 0, 0, 0, 0, 0, 0, GLFW_WINDOW)) {
+        throw std::runtime_error("Could not open GLFW window!");
+    }
+
+    // Run test
+    try {
+        FloatUniformNodeTest test;
+        test.testGetType();
+        test.testVisit();
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        throw;
+    }
+
+    // Exit
+    glfwTerminate();
+    return 0;
+}
