59fe1d9f44c77bc7ad3033eec6ed19e6d197c56d
Andy Brown <adb1413@rit.edu>
2013-02-03 02:15:51 -0500

    Explicitly include stdexcept in ClearNodeUnmarshaller (ref #6)

--- RapidGL/ClearNodeUnmarshaller.cxx
+++ RapidGL/ClearNodeUnmarshaller.cxx
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "config.h"
+#include <stdexcept>
 #include "RapidGL/ClearNodeUnmarshaller.h"
 using namespace std;
 namespace RapidGL {
